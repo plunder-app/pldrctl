99e81573ee5d4b690179c55bfe68d20b2aa2cac3
Dan Finneran <daniel.finneran@gmail.com>
2019-08-08 12:30:05 +0100

    Readonly support for pldrctl now works with encrypted comms

---
+++ pkg/ux/server.go
@@ -0,0 +1,34 @@
+package ux
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/plunder-app/plunder/pkg/services"
+)
+
+//ServerFormat will display the global deployment configuration for a Plunder Server
+func ServerFormat(b services.BootController) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+
+	fmt.Fprintf(w, "%s:\t%s\n", "Adapter", *b.AdapterName)
+	fmt.Fprintf(w, "%s:\t%t\n", "Enable DHCP", *b.EnableDHCP)
+	// DNS Configuration
+	fmt.Fprintf(w, "\t%s:\t%s\n", "DHCP Start Address", *b.DHCPConfig.DHCPStartAddress)
+	fmt.Fprintf(w, "\t%s:\t%s\n", "DHCP Server Address", *b.DHCPConfig.DHCPAddress)
+	fmt.Fprintf(w, "\t%s:\t%s\n", "DHCP Gateway Address", *b.DHCPConfig.DHCPGateway)
+	fmt.Fprintf(w, "\t%s:\t%s\n", "DHCP DNS Address", *b.DHCPConfig.DHCPDNS)
+	fmt.Fprintf(w, "\t%s:\t%d\n", "DHCP Lease Pool Size", *b.DHCPConfig.DHCPLeasePool)
+	fmt.Fprintf(w, "%s:\t%t\n", "Enable TFTP", *b.EnableTFTP)
+	// TFTP Configuration
+	fmt.Fprintf(w, "\t%s:\t%s\n", "TFTP Server Address", *b.TFTPAddress)
+
+	fmt.Fprintf(w, "%s:\t%t\n", "Enable HTTP", *b.EnableHTTP)
+	// TFTP Configuration
+	fmt.Fprintf(w, "\t%s:\t%s\n", "DHCP Server Address", *b.HTTPAddress)
+	fmt.Fprintf(w, "\t%s:\t%s\n", "PXE File Name", *b.PXEFileName)
+
+	w.Flush()
+
+}
